7ed498582ab41106befd720eefd88bfadbfc751c
johnTheRipOff <114521400+johnTheRipOff@users.noreply.github.com>
2023-12-22 15:17:15 -0500

    Add files via upload

---
+++ patient/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('patientIntake', views.patientIntake, name='patientIntake'),
+    path('consultationScheduler', views.consultationScheduler, name='consultationScheduler'),
+    path('staffDirectory', views.staffDirectory, name='staffDirecotry'),
+    path('inPatientCare', views.inPatientCare, name='inPatientCare'),
+    path('doctorSchedule', views.doctorSchedule, name='doctorSchedule'),
+    path('jobShfit', views.jobShift, name='jobShiftViewer'),
+    path('surgery',views.surgery, name='surgery')
+
+]
